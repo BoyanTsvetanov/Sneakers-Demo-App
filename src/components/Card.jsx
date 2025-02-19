4e4245c436080c86f4e17fc23025c45cf152c2bc
BoyoTheProg <136482603+BoyoTheProg@users.noreply.github.com>
2025-02-19 17:13:14 +0200

    Updated products section

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Card = ({imgUrl, name, price}) => {
   return (
-    <div className='flex flex-col bg-dark rounded-xl px-4 py-3 '>
-        <img src={imgUrl} alt="shoe" className='h-full object-contain' />
-        <div className=''>
+    <div className='flex flex-col bg-dark rounded-xl px-4 py-3 group cursor-pointer'>
+        <img src={imgUrl} alt="shoe" className='h-full object-contain group-hover:scale-115 transition-all duration-500' />
+        <div className='h-[50%]'>
             <p className='max-sm:text-xs text-sm font-roboto max-sm:font-bold'>{name}</p>
             <p className='text-2xl font-bold'>{price}</p>
         </div>    
--- src/sections/Products.jsx
+++ src/sections/Products.jsx
@@ -1,27 +1,35 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { products, sneaker_categories } from '../constants'
 import Card from '../components/Card'
+import clsx from 'clsx';
 
 const Products = () => {
+    const [filter, setFilter] = useState(false);
+
+
   return (
     <section className='relative h-screen' name="Продукти">
         <h1 className='ml-16 pl-1 mt-8 text-2xl border-l-accent-magenta border-l-4 font-bebas font-bold'>Продукти</h1>
 
-        <ul className="absolute mt-8 mx-16 max-md:mx-8 rounded-3xl p-1 flex gap-6 bg-gradient-to-br from-[hsl(327,100%,45%)] to-[hsl(259,77%,25%)]">
-            <div className="bg-dark rounded-3xl p-4 h-min max-sm:hidden">
+        <div className="absolute mt-8 mx-16 max-md:mx-8 rounded-3xl p-1 flex gap-6 max-sm:gap-1.5 bg-gradient-to-br from-[hsl(327,100%,45%)] to-[hsl(259,77%,25%)]">
+            <button className='hidden max-sm:inline-block z-10 bg-light w-14 h-min rounded-full mt-1 ml-1' onClick={() => setFilter((prevState) => !prevState)}>
+                <img src={`./icons/${filter? 'close' : 'hamburger'}.svg`} alt="menu" className='w-full h-full object-contain p-2' />
+            </button>
+
+            <ul className={clsx("bg-dark rounded-3xl p-4 h-min ", filter ? 'absolute block' : 'max-sm:hidden')}>
                 {sneaker_categories.map((category) => (
-                <li key={category.id} className="mx-8 my-4 pb-4 border-b-2 border-light/25 last:border-b-0">
-                    <button>{category.name}</button>
+                <li key={category.id} className={clsx("mx-8 my-4 pb-4 border-b-2 border-light/25 last:border-b-2 hover:border-light transition-all duration-500", filter && 'first:mt-10')}>
+                    <button className='cursor-pointer'>{category.name}</button>
                 </li>
                 ))}
-            </div>
+            </ul>
 
-            <div className='right-10 w-full rounded-3xl p-1 grid grid-cols-[repeat(auto-fit,minmax(150px,2fr))] gap-3'>
+            <div className='right-10 w-full rounded-3xl p-1 grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] max-lg:grid-cols-[repeat(auto-fit,minmax(150px,1fr))] max-sm:grid-cols-[repeat(auto-fit,minmax(125px,1fr))] gap-3'>
             {products.map((shoe) => (
                 <Card imgUrl={shoe.imgURL} name={shoe.name} price={shoe.price}></Card>
             ))}
         </div>
-        </ul>
+        </div>
 
 
     </section>
